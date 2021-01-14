925c86359131f63fc090761b98158454f9eb8d9a
Himanshu Raj <himanshurajkush@gmail.com>
2021-01-14 20:12:55 +0530

    OmniPhoenix: Cleanup

--- omni_phoenixin.mk
+++ omni_phoenixin.mk
@@ -19,20 +19,12 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from phoenixin device
 $(call inherit-product, device/xiaomi/phoenixin/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := phoenixin
-PRODUCT_NAME := omni_phoenixin
-PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO X2
-PRODUCT_MANUFACTURER := xiaomi
-PRODUCT_RELEASE_NAME := POCO POCO X2
