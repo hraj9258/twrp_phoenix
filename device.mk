c2b12e703e2b8ee378fd8cf03b147a89401941da
Himanshu Raj <himanshurajkush@gmail.com>
2021-01-14 20:12:17 +0530

    device: added Qcom Decryption, fastboot, apex lib

--- device.mk
+++ device.mk
@@ -18,3 +18,32 @@
 
 LOCAL_PATH := device/xiaomi/phoenixin
 
+# Qcom standerd Decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+	android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
+
+# Apex libraries
+PRODUCT_HOST_PACKAGES += \
+    libandroidicu
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := phoenixin
+PRODUCT_NAME := omni_phoenixin
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO X2
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_RELEASE_NAME := POCO X2
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
