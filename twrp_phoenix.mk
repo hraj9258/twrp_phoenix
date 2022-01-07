05088bc8c4cec28ca2f282972ee163fa86dd364a
Himanshu Raj <himanshurajkush@gmail.com>
2022-01-07 19:30:01 +0530

    Prebuilt: Updated Prebuilts from miui 12.5.6

--- twrp_phoenix.mk
+++ twrp_phoenix.mk
@@ -23,7 +23,7 @@ $(call inherit-product, device/xiaomi/phoenix/device.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
-    $(LOCAL_PATH)/prebuilt/dtb:dtb.img
+    $(LOCAL_PATH)/prebuilt/dtb.img:dtb.img
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := phoenix
